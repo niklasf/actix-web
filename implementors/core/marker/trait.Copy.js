adf026924c115cc26319c8a845ea6bd3786b56c2
Travis CI User <travis@example.org>
2018-03-02 04:37:54 +0000

    Documentation upload

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["actix_web"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"actix_web/headers/enum.ConnectionType.html\" title=\"enum actix_web::headers::ConnectionType\">ConnectionType</a>",synthetic:false,types:["actix_web::httpresponse::ConnectionType"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"actix_web/ws/enum.CloseCode.html\" title=\"enum actix_web::ws::CloseCode\">CloseCode</a>",synthetic:false,types:["actix_web::ws::proto::CloseCode"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"actix_web/httpcodes/struct.StaticResponse.html\" title=\"struct actix_web::httpcodes::StaticResponse\">StaticResponse</a>",synthetic:false,types:["actix_web::httpcodes::StaticResponse"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"actix_web/headers/enum.ContentEncoding.html\" title=\"enum actix_web::headers::ContentEncoding\">ContentEncoding</a>",synthetic:false,types:["actix_web::headers::ContentEncoding"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
